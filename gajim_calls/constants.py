e9801e81e5f26bb2b5bf8518d0bdec67427bf2f2
GigabiteStudios <gigabitestudios@noreply.localhost>
2026-08-20 00:05:54 +0000

    feat: add protocol namespaces

---
+++ gajim_calls/constants.py
@@ -0,0 +1,13 @@
+"""Protocol namespaces and plugin constants."""
+
+PLUGIN_SHORT_NAME = "gajim_calls"
+
+NS_JINGLE = "urn:xmpp:jingle:1"
+NS_JMI = "urn:xmpp:jingle-message:0"
+NS_RTP = "urn:xmpp:jingle:apps:rtp:1"
+NS_RTP_INFO = "urn:xmpp:jingle:apps:rtp:info:1"
+NS_RTCP_FB = "urn:xmpp:jingle:apps:rtp:rtcp-fb:0"
+NS_ICE_UDP = "urn:xmpp:jingle:transports:ice-udp:1"
+NS_DTLS = "urn:xmpp:jingle:apps:dtls:0"
+NS_GROUPING = "urn:xmpp:jingle:apps:grouping:0"
+NS_HINTS = "urn:xmpp:hints"
